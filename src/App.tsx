ba703726bf4fe8d3191a60bc6cc18661512a8e69
ula28 <ula24499@gmail.com>
2023-08-30 09:58:12 +0200

    lesson3

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,22 @@
 import './App.css';
-import styled from 'styled-components';
+import React from 'react';
+import cat from '../src/assets/images/cat-1.jpg'
+import cat2 from '../src/assets/images/cat-2.jpg'
 
 
 function App() {
     return (
         <div className="App">
-            <Title>Styled-components <span>.attrs</span> method</Title>
+            <p>Hello, Retina &#128075;</p>
 
-            <Form>
-                <Field type ="checkbox"/>
-                <Field/>
-                <Field/>
-					 {/* <Checkbox/> */}
-            </Form>
+            {/*<div className={'bg'}></div>*/}
+            {/*<img src={cat2} alt="cat"/>*/}
+
         </div>
     );
 }
 
 export default App;
 
-const Title = styled.h1`
-    font-size: 1.5em;
-    text-align: center;
-    margin-bottom: 32px;
-    span {
-      color: #e91e63;
-    }
-`;
 
-const Form = styled.form`
-  width: 100%;
-  max-width: 500px;
-`;
 
-const Field = styled.input`
-  padding: 5px 15px;
-  margin: 10px 0;
-  width: 100%;
-  font-size: 1rem;
-`;
-
-// const Checkbox = styled.input.attrs((props)=> ({
-// 	type: props.type || 'checkbox',
-// 	title: props.title
-// }))`
-//  padding: 5px 15px;
-//  margin: 10px 0;
-//  width: 100%;
-//  font-size: 1rem;
-// `;
